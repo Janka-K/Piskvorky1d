9b4bc9ba4f2bb0df86c0084e359a6d9b6b0a3f88
Janka-K <69305418+Janka-K@users.noreply.github.com>
2020-11-13 21:31:47 +0100

    Update funkcepiskvorky_1d.py

--- funkcepiskvorky_1d.py
+++ funkcepiskvorky_1d.py
@@ -1,6 +1,3 @@
-#Napiš funkci piskvorky1d, která vytvoří řetězec s herním polem a střídavě volá #funkce tah_hrace a
-#tah_pocitace, dokud někdo nevyhraje nebo nedojde k remíze.
-#Nezapomeň kontrolovat stav hry po každém tahu.
 
 from funkcetah_hrace import tah_hrace
 from funkcetahpocitace import tah_pocitace
