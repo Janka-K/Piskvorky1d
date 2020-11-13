16558c1bfd6159af1f75b1d8aff8b4eeb7a2980d
Janka-K <69305418+Janka-K@users.noreply.github.com>
2020-11-13 21:31:13 +0100

    Add files via upload

---
+++ funkcepiskvorky_1d.py
@@ -0,0 +1,45 @@
+#Napiš funkci piskvorky1d, která vytvoří řetězec s herním polem a střídavě volá #funkce tah_hrace a
+#tah_pocitace, dokud někdo nevyhraje nebo nedojde k remíze.
+#Nezapomeň kontrolovat stav hry po každém tahu.
+
+from funkcetah_hrace import tah_hrace
+from funkcetahpocitace import tah_pocitace
+from funkcevyhodnot import vyhodnot
+
+
+def piskvorky_1d():
+  herni_pole = '-' * 20
+  print(herni_pole)
+  hra_bezi = True
+  while hra_bezi:
+    herni_pole = tah_hrace(herni_pole)
+    hodnoceni = vyhodnot(herni_pole)
+    if hodnoceni == "-":
+      print(f"Hra pokracuje")
+    elif hodnoceni == "x" or hodnoceni == "o":
+      hra_bezi = False
+      print(herni_pole)
+      print(f"Vyhrava {hodnoceni}")
+      continue
+    else:
+      hra_bezi = False
+      print("Remiza")
+    herni_pole = tah_pocitace(herni_pole)
+    hodnoceni = vyhodnot(herni_pole)
+    print(herni_pole)
+    if hodnoceni == "-":
+      print(f"Hra pokracuje")
+    elif hodnoceni == "x" or hodnoceni == "o":
+      hra_bezi = False
+      print(herni_pole)
+      print(f"Vyhrava {hodnoceni}")
+      continue
+    else:
+      hra_bezi = False
+      print("Remiza")
+
+#napsat funkci na nahrazeni podminky, aby se podminka v cyklu neopakovala
+
+piskvorky_1d()
+
+
